d7bbbf28d9880dbb97723b92d8d4044ad03d9fc4
Bill Cook <billcook8122@gmail.com>
2023-06-23 14:43:25 +0900

    env added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import AllMovies from './components/all-movies';
 import { Movie } from './types';
 // Index file in Next 13
-// Things from Mantine do not work well on server side. Therefore, must be inside a seperate component.
 async function getMovies() {
-  const res = await fetch('https://ghibliapi.vercel.app/films')
-  { cache: 'no-store'}
+  const apiUrl = process.env.NEXT_PUBLIC_GHILIBI_API
+
+  if (!apiUrl) {
+    console.error('API URL is not defined');
+    return [];
+  }
+  const res = await fetch(apiUrl, { cache: 'no-store' })
   const data = await res.json()
   return data as Movie[]
 }
