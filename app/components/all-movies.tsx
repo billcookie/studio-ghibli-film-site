4b6595f4571760772e0861ef8031d1d4a7a922d0
Bill Cook <billcook8122@gmail.com>
2023-06-23 21:51:36 +0900

    search with pagination

--- app/components/all-movies.tsx
+++ app/components/all-movies.tsx
@@ -13,11 +13,20 @@ interface AllMoviesProps {
 const AllMovies: React.FC<AllMoviesProps> = ({
   data,
 }) => {
-  const { movies, totalPages, currentPage, handlePageChange } = usePagination(data, 10)
-  const [searchQuery, setSearchQuery] = useState("")
-  const handleSearchQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(event.target.value)
-  }
+  const {
+    movies,
+    totalPages,
+    currentPage,
+    handlePageChange,
+    handleSearch,
+  } = usePagination(data, 10);
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+    handleSearch(e.target.value);
+  };
   const filteredMovies = movies.filter((movie) =>
     movie.title.toLowerCase().includes(searchQuery.toLowerCase())
   );
@@ -27,7 +36,7 @@ const AllMovies: React.FC<AllMoviesProps> = ({
         <Input
           placeholder="Search movies..."
           value={searchQuery}
-          onChange={handleSearchQueryChange}
+          onChange={handleSearchInput}
         />
       </div>
       <div className="grid grid-cols-1 gap-0 sm:grid-cols-2 sm:gap-16">
@@ -37,13 +46,13 @@ const AllMovies: React.FC<AllMoviesProps> = ({
 
       </div>
       <div>
-          <Pagination
-            total={totalPages}
-            value={currentPage}
-            onChange={handlePageChange}
-            size="lg"
-          />
-        </div>
+        <Pagination
+          total={totalPages}
+          value={currentPage}
+          onChange={handlePageChange}
+          size="lg"
+        />
+      </div>
     </>
   );
 }
--- app/hooks/usePagination.ts
+++ app/hooks/usePagination.ts
@@ -1,17 +1,37 @@
 import { useEffect, useState } from 'react';
 import { Movie } from '../types';
 
-export const usePagination = (movies: Movie[], perPage: number) => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const indexOfLastMovie = currentPage * perPage;
-  const indexOfFirstMovie = indexOfLastMovie - perPage;
-  const currentMovies = movies.slice(indexOfFirstMovie, indexOfLastMovie);
-
-  const totalPages = Math.ceil(movies.length / perPage);
+export const usePagination = (data: Movie[], itemsPerPage: number) => {
+  const [currentPage, setCurrentPage] = useState(1)
+  const [searchQuery, setSearchQuery] = useState("")
 
   const handlePageChange = (newPage: number) => {
-    setCurrentPage(newPage);
+    setCurrentPage(newPage)
   };
 
-  return { movies: currentMovies, totalPages, currentPage, handlePageChange };
+  const handleSearch = (query: string) => {
+    setSearchQuery(query)
+    setCurrentPage(1)
+  };
+
+  const filteredMovies = data.filter((movie) =>
+    movie.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const indexOfLastMovie = currentPage * itemsPerPage
+  const indexOfFirstMovie = indexOfLastMovie - itemsPerPage
+  const currentMovies = filteredMovies.slice(
+    indexOfFirstMovie,
+    indexOfLastMovie
+  );
+
+  const totalPages = Math.ceil(filteredMovies.length / itemsPerPage);
+
+  return {
+    movies: currentMovies,
+    totalPages,
+    currentPage,
+    handlePageChange,
+    handleSearch,
+  };
 };
