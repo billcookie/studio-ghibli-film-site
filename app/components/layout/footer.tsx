5ce362b9e805825eb01cfe7b6a34352e83e6cf22
Bill Cook <billcook8122@gmail.com>
2023-06-25 23:57:00 +0900

    image fix

--- app/components/layout/footer.tsx
+++ app/components/layout/footer.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { Image } from '@mantine/core'
 import Link from 'next/link'
 import { AiFillFacebook, AiFillInstagram, AiFillYoutube, AiFillTwitterSquare } from 'react-icons/ai'
 
@@ -11,7 +10,6 @@ const Footer = ({
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <Link href="/" className="flex items-center mb-4 sm:mb-0">
-            {/* <Image src="/logo.png" className="h-8 mr-3" alt="Logo" width={50} height={50} /> */}
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Ghibliography</span>
           </Link>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
